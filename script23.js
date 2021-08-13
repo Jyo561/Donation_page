2fa4552e6af8797cef26d1e59f9e730bcba952f4
Jyo561 <jyotiradityakuanar34@gmail.com>
2021-08-13 14:14:20 +0530

    changes

---
+++ script23.js
@@ -0,0 +1,15 @@
+ $("#nav1").click( function(){
+   $("#nav1").css("background-color","white");
+   $("#nav2").css("background-color","#ffc107");
+   $("#nav3").css("background-color","#ffc107");
+ });
+$("#nav2").click( function(){
+   $("#nav1").css("background-color","#ffc107");
+   $("#nav2").css("background-color","white");
+   $("#nav3").css("background-color","#ffc107");
+ });
+$("#nav3").click( function(){
+   $("#nav1").css("background-color","#ffc107");
+   $("#nav2").css("background-color","#ffc107");
+   $("#nav3").css("background-color","white");
+ });
